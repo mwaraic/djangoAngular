4aa4ec00c1dbea1a52c9ed72be2f1ab0cf33882c
mwaraic <59263215+mwaraic@users.noreply.github.com>
2021-08-26 18:15:45 -0400

    template made for frontend and backend

--- django/MyProject/MyProject/urls.py
+++ django/MyProject/MyProject/urls.py
@@ -1,7 +1,7 @@
+  
 """MyProject URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,23 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from api_basic.views import CustomUserCreate
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path,include
-from api_basic.views import ArticleViewSet, CustomTokenObtainPairView
+from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/article/', ArticleViewSet.as_view({'get':'list', 'post':'create'})),
-    path('api/article/<int:pk>', ArticleViewSet.as_view({'get':'retrieve','put':'update', 'delete':'destroy'})),
-    path('api/auth/register/', CustomUserCreate.as_view()),
-    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+    path(r'', include('microblog.urls')),
+    path(r'api-token-auth/', TokenObtainPairView),
+    path(r'api-token-refresh/',TokenRefreshView),
 ]
