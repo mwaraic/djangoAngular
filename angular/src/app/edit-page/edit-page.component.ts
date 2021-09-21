23975a74fdc5ebaad3fa6cd06549fbda7921bf8f
azr <azriasat@gmail.com>
2021-09-21 16:31:07 -0400

    edit page recent updates

--- angular/src/app/edit-page/edit-page.component.ts
+++ angular/src/app/edit-page/edit-page.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
+import { NgModule } from '@angular/core';
+
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.css']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnChanges, OnInit {
+heading:string='';
+myVa:boolean=true;
 
   constructor() { }
+ngOnInit():void{
+  document.designMode = "on";
+  this.heading='Hey there, I am web-Developer|'
+}
+  ngOnChanges() {
+    alert('value changed');
 
-  ngOnInit(): void {
-  }
-  getFormValue(val){
-    console.log(val)
-  }
 
 }
+
+    
+  }
+  
+
+
